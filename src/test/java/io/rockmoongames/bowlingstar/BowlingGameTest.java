faf0254e8a513131380d8d9cdfdb0b3b2bb94f1b
Nada-Marghany <nada.marghany@vodafone.com>
2022-06-26 17:58:56 +0200

    add a seconed test

--- src/test/java/io/rockmoongames/bowlingstar/BowlingGameTest.java
+++ src/test/java/io/rockmoongames/bowlingstar/BowlingGameTest.java
@@ -1,5 +1,16 @@
 package io.rockmoongames.bowlingstar;
 
+import org.testng.annotations.Test;
+
 public class BowlingGameTest
 {
+    @Test
+    public void jessicaTestOne(){
+
+    }
+
+    @Test
+    public void jessicaTestTwo(){
+
+    }
 }
